50267e2ecde16ddfb221fda2e0175c492a48998a
s163025 <s163025@matsu.shimane-u.ac.jp>
2018-11-17 05:15:26 +0000

    値の表示 and 変数定義

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,38 +1,26 @@
 class HomeController < ApplicationController
     
     def about
-      @Level1=1000
-      @Level2=1500
-      @Level3=2000
-      @Level4=2500
-      @Level5=3000
-      @Level6=3500
-      @Level7=4000
-      @Level8=4500
-      @Level9=5000
-      @Level10=5500
-      @Level11=6000
-      @Level12=6500
-      @Level13=7000
-      @Level14=7500
-      @Level15=8000
-      @Level16=8500
-      @Level17=9000
-      @Level18=9500
-      @Level19=10000
-      @Level20=10500
-      @Level21=11000
-      @Level22=11500
-      @Level23=12000
-      @Level24=12500
-      @Level25=13000
-      @Level26=13500
-      @Level27=14000
-      @Level28=14500
-      @Level29=15000
-      @Level30=15500
+      @Level= [
+          0,
+          1000,
+          1500,
+          2000,
+          2500,
+          3000,
+          3500,
+          4000,
+          4500,
+          5000,
+          5500,
+          6000]
       
       @user=User.find(1)
+      
+      experience_sum=@Level.slice(0,@user.level).sum
+      
+      @now_experience = @user.experience - experience_sum
+      @next_experience = @Level[@user.level] - @now_experience
     end 
     
     def breaktime
@@ -48,38 +36,26 @@ class HomeController < ApplicationController
     end
     
     def result
-     @Level1=1000
-      @Level2=1500
-      @Level3=2000
-      @Level4=2500
-      @Level5=3000
-      @Level6=3500
-      @Level7=4000
-      @Level8=4500
-      @Level9=5000
-      @Level10=5500
-      @Level11=6000
-      @Level12=6500
-      @Level13=7000
-      @Level14=7500
-      @Level15=8000
-      @Level16=8500
-      @Level17=9000
-      @Level18=9500
-      @Level19=10000
-      @Level20=10500
-      @Level21=11000
-      @Level22=11500
-      @Level23=12000
-      @Level24=12500
-      @Level25=13000
-      @Level26=13500
-      @Level27=14000
-      @Level28=14500
-      @Level29=15000
-      @Level30=15500
+    @Level= [
+          0,
+          1000,
+          1500,
+          2000,
+          2500,
+          3000,
+          3500,
+          4000,
+          4500,
+          5000,
+          5500,
+          6000]
       
       @user=User.find(1)
+      
+      experience_sum=@Level.slice(0,@user.level).sum
+      
+      @now_experience = @user.experience - experience_sum
+      @next_experience = @Level[@user.level] - @now_experience
     end
     
     def start
